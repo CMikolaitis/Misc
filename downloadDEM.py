0b2eecd163fdfa71c033a1e52b9d05510236a049
C. Mikolaitis <133790086+CMikolaitis@users.noreply.github.com>
2026-07-16 19:46:30 -0500

    Update downloadDEM.py

--- downloadDEM.py
+++ downloadDEM.py
@@ -1,31 +1,39 @@
 import requests
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
 
-here = Path(".")
+# Handle pathing
+here    = Path(".")
+destDir = here / Path("DEM_W")
+destDir.mkdir(parents=True, exist_ok=True)
 
+# Func to download
+def download(url):
+    filename = Path(url).name
+    outfile = destDir / filename
+
+    if outfile.exists():
+        print(f"Skipping {filename} (already exists)")
+        return
+
+    try:
+        r = requests.get(url, stream=True, timeout=30)
+        r.raise_for_status()
+
+        with open(outfile, "wb") as out:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    out.write(chunk)
+
+        print(f"Downloaded {filename}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Failed {filename}: {e}")
+
+# Read in file with all download urls, needs to be newline for each
 with open("MobileCountyDEM2023.txt") as f:
-    for line in f:
-        url = line.strip()
-        if not url:
-            continue
+    urls = [line.strip() for line in f if line.strip()]
 
-        filename = Path(url).name
-        outfile = here / filename
-        
-        if outfile.exists():
-            print(f"Skipping {filename} (already exists)")
-            continue
-        
-        try:
-            r = requests.get(url, stream=True, timeout=30)
-            r.raise_for_status()
-
-            with open(outfile, "wb") as out:
-                for chunk in r.iter_content(chunk_size=8192):
-                    if chunk:  # filter out keep-alive chunks
-                        out.write(chunk)
-
-            print(f"Downloaded {filename}")
-
-        except requests.exceptions.RequestException as e:
-            print(f"Failed {filename}: {e}")
+# Download multiple at once
+with ThreadPoolExecutor(max_workers=8) as pool:
+    pool.map(download, urls)
